a338e4f070e470ec2d7967989876cfcebe8bcaa2
Jacob Brenke <jacob.brenke@gmail.com>
2020-04-22 13:09:33 -0500

    Finalize if else boolean statement

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -1,13 +1,11 @@
 let parkedCars = []; //declare empty array parkedCars.
-const maxCars = 5;  //declare variable maxCars.
+const maxCars = 5; //declare variable maxCars.
 
-function parkCar ( carName ) {
+function parkCar(carName) {
   console.log('inside parkCar');
-  if(parkedCars.length < maxCars){
-    parkedCars.push(carName);
+  if (parkedCars.length < maxCars) {
     return true;
-  }
-  else{
-      return false;
-} //end else if check
+  } else {
+    return false;
+  } //end else if check
 } //end function parkCar.

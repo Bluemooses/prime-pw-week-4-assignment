95952f758848f0a1d278082ae1a2fdb33a2aff9e
Jacob Brenke <jacob.brenke@gmail.com>
2020-04-22 13:36:49 -0500

    Adjusting function isParkedHere to show boolean true value

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -23,8 +23,15 @@ console.log(parkCar());
 
 
 function isParkedHere( carNamed) {
+if(parkedCars.includes(carName) === carNamed){
 
+  return true;
+}
+else{
+  return false;
+}
 }//ends function isParkedHere.
+isParkedHere( 'Versa');
 
 function takeForASpin (carName) {
 

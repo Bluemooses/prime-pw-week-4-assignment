0583aab27d8599e49e4715ed8ff5f7f8c8fa57da
Jacob Brenke <jacob.brenke@gmail.com>
2020-04-22 11:52:42 -0500

    Check if garage has space for more cars.

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -1,4 +1,9 @@
 console.log('js');
 
 let parkedCars = [];  //create empty array of cars parked in garage.
-const maxCars = 5;
+const maxCars = 5;    //create constant for max cars allowed in garage.
+
+function parkCar('Jetta Wagon'){   //creates function parkCar of my VW Jetta Wagon.
+      parkedCars.length() < maxCars;  //checks length of array versus the maxCars.
+      
+}

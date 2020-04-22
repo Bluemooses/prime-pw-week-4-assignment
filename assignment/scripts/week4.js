d61e0c9501d37fcc628a7ce6e9168bd047bd50af
Jacob Brenke <jacob.brenke@gmail.com>
2020-04-22 12:45:31 -0500

    Adding function parkCar

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -1,2 +1,8 @@
-let parkedCars = [];
-const maxCars = 5;
+let parkedCars = []; //declare empty array parkedCars.
+const maxCars = 5;  //declare variable maxCars.
+
+function parkCar ( carName ) {
+  if(parkedCars.length < maxCars){
+
+  }//end if else check.
+} //end function parkCar.

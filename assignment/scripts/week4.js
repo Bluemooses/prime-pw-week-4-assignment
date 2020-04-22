43c15ec0c8076343bb03f2897795cba705738ccd
Jacob Brenke <jacob.brenke@gmail.com>
2020-04-22 12:47:27 -0500

    Adding parkCar values

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -3,6 +3,14 @@ const maxCars = 5;  //declare variable maxCars.
 
 function parkCar ( carName ) {
   if(parkedCars.length < maxCars){
+    return true;
+    parkedCars.push(carName);
+
 
   }//end if else check.
 } //end function parkCar.
+parkCar('Jetta Wagon');
+parkCar('Nissan Versa');
+parkCar('Honda Civic');
+parkCar('Honda Fit');
+parkCar('Subaru Outback');

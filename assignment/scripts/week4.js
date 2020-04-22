f78382b2a3dbbd94c60674ea471bdfca641f5de2
Jacob Brenke <jacob.brenke@gmail.com>
2020-04-22 11:58:54 -0500

    Creating a check if my function is visible in console.

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -3,7 +3,6 @@ console.log('js');
 let parkedCars = []; //create empty array of cars parked in garage.
 const maxCars = 5; //create constant for max cars allowed in garage.
 
-function parkCar('Jetta Wagon') { //creates function parkCar of my VW Jetta Wagon.
-  parkedCars.length() < maxCars; //checks length of array versus the maxCars.
-
-}
+function parkCar(){
+console.log( 'Checking if I can park my car!'); //let's us know if we are in this function.
+} //end parkCar

635236e61bba00a34dffe60a733a7d95aca5683f
Jacob Brenke <jacob.brenke@gmail.com>
2020-04-23 10:04:46 -0500

    creating array and variable for function parkCar

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -1,38 +1,6 @@
-let parkedCars = []; //declare empty array parkedCars.
-const maxCars = 5; //declare variable maxCars.
+let parkedCars = [];  //creating parkedCars array
+const maxCars = 5;  //const maxCars allowed
 
-function parkCar(carName) {
-  console.log('inside parkCar', carName);
-  if (maxCars < parkedCars.length) {
-    return true;
-    parkedCars.push(carName);
-    return parkedCars;
-  } else {
-    return false;
-  } //end else if check
-} //end function parkCar.
-
-parkCar('Versa');
-parkCar('Volvo');
-parkCar('Cadillac');
-parkCar('Volkswagon');
-parkCar('Mini Cooper');
-parkCar('Hyundai Sonata');
-console.log(parkedCars);
-console.log(parkCar());
-
-
-function isParkedHere( carNamed) {
-if(parkedCars.includes(carName) === carNamed){
-
-  return true;
-}
-else{
-  return false;
-}
-}//ends function isParkedHere.
-isParkedHere( 'Versa');
-
-function takeForASpin (carName) {
-
-} //ends takeForASpin.
+function parkCar( carName ){
+  
+}//ending parkCar function

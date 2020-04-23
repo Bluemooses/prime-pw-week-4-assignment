f0c924aab498f6a9ff0481bca793ed337b869f6e
Jacob Brenke <jacob.brenke@gmail.com>
2020-04-23 10:14:26 -0500

    creating the parkCar function

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -2,5 +2,11 @@ let parkedCars = [];  //creating parkedCars array
 const maxCars = 5;  //const maxCars allowed
 
 function parkCar( carName ){
-  
+  if(maxCars > parkedCars.length){
+    parkedCars.push(carName);
+    return true;
+  } //ending if statement
+  else{
+    return false;
+  } //ending else statement
 }//ending parkCar function

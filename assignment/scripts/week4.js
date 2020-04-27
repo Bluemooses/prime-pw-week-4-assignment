c221a5f0725e4851114e02376830aea130862d86
Jacob Brenke <jacob.brenke@gmail.com>
2020-04-27 14:25:32 -0500

    Updated stretch goal functionality with same principals in isParkedHere

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -1,55 +1,56 @@
-let parkedCars = [];  //creating parkedCars array
-const maxCars = 5;  //const maxCars allowed
+let parkedCars = []; //creating parkedCars array
+const maxCars = 5; //const maxCars allowed
 
-function parkCar( carName, color, vin ){ //creates function parkCar with an argument of carName.
-  if(maxCars > parkedCars.length){  //is there room in the garage to park the carName?
-    parkedCars.push({model:carName, color: color, vin:vin}); //pushes argument parkCar to parkedCar
-    return true;  //returns true if a carName was pushed.
+function parkCar(carName, color, vin) { //creates function parkCar with an argument of carName.
+  if (maxCars > parkedCars.length) { //is there room in the garage to park the carName?
+    parkedCars.push({
+      model: carName,
+      color: color,
+      vin: vin
+    }); //pushes argument parkCar to parkedCar
+    return true; //returns true if a carName was pushed.
   } //ending if statement
-  else{
+  else {
     return false;
   } //ending else statement
-}//ending parkCar function
+} //ending parkCar function
 
-parkCar( "Chevy", "Green", "654798752");
-parkCar( "Tesla", "Gold", "65479");
+parkCar("Chevy", "Green", "654798752");
+parkCar("Tesla", "Gold", "65479");
 parkCar("Honda", "Brown", "654798");
 parkCar("Nissan", "Blue", "7898796874685546");
 parkCar("Volvo", "Red", "654654654654");
 parkCar("Ford", "Purple", "65465465411321");
 
 
-function isParkedHere( searchTerm){  //creates function isParkedHere with argument searchTerm.
-  if(parkedCars.some(car => car.model === searchTerm || car.color === searchTerm || car.vin === searchTerm)){
+function isParkedHere(searchTerm) { //creates function isParkedHere with argument searchTerm.
+  if (parkedCars.some(car => car.model === searchTerm || car.color === searchTerm || car.vin === searchTerm)) {
     console.log("We found a match", searchTerm);
     return true; //returns true if a carNamed is parked in garage
-  }
-  else{
+  } else {
     console.log("We didn't find what we're looking for", searchTerm);
     return false; //returns false if that car isn't parked in garage.
   }
-}//ends isParkedHere function
+} //ends isParkedHere function
 
-// console.log(isParkedHere("Chevy Impala")); //calls isParkedHere function with argument in console.
-// console.log(isParkedHere("Tesla X"));
-// console.log(isParkedHere("Honda Accord"));
-// console.log(isParkedHere("Turbo Diesel Z500"));
+console.log(isParkedHere("Chevy Impala")); //calls isParkedHere function with argument in console.
+console.log(isParkedHere("Tesla X"));
+console.log(isParkedHere("Honda Accord"));
+console.log(isParkedHere("Turbo Diesel Z500"));
 
-function takeForASpin( searchTerm ){ //creates function takeForASpin with argument carName
+function takeForASpin(searchTerm) { //creates function takeForASpin with argument carName
   console.log("parked cars =", parkedCars);
-  if(isParkedHere( searchTerm )){ //creates if argument on searchTerm
-    let parkingSpaceNumber = parkedCars.indexOf(searchTerm); //creates variable to check index of our searchTerm in parkedCars array
-    console.log("parkingSpaceNumber =", parkingSpaceNumber); //logging that value
-    parkedCars.splice(parkingSpaceNumber, 1);
-    console.log("parked cars =", parkedCars); //logs parkedCar.
-    console.log(searchTerm);  //logs our searchTerm.
-    return searchTerm;  //returns searchTerm if if argument is true!
-  } //ends our if check
-    else{
-      return false; //returns boolean false if we can't take our searchTerm for a spin!
-    };
-}//ends function takeForASpin
+  if( parkedCars.indexOf(car => car.model === searchTerm || car.color === searchTerm || car.vin === searchTerm)){
+    searchTerm > -1;
+    parkedCars.splice(searchTerm, 1);
+    return searchTerm;
+    return parkedCars;
+    console.log("Take er' for a rip!", searchTerm);
+  }
+  else{
+    return false;
+  }};
 
-console.log(parkedCars); //logging our cars parked in the garage.
-takeForASpin( "Chevy" ); //should return our searchTerm.
-takeForASpin( "Delorean" ); //should return false.
+  takeForASpin("Delorean"); //not parked in garage, false.
+  takeForASpin("Purple"); //returns truth.
+  takeForASpin("Ford");  //should return false as purple is out of array.
